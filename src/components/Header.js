70b2310254b74869124db36029244bf414e8f795
Edgarcarrog <edgc21@hotmail.com>
2021-07-22 19:28:26 -0500

    Header and Nav components

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="row">
+        <div className="col">
+          <a href="!#" className="anchor">
+            <p>colectivochicome@gmail.com</p>
+          </a>
+        </div>
+      </div>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
